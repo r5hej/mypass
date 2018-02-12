d5310431a7868ab4085972bebd11ff90ea943fe2
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2018-02-12 14:57:47 +0100

    register done?
    renamed routes to index

--- public/register/main.js
+++ public/register/main.js
@@ -2,30 +2,54 @@
 
 
 let form = document.getElementById('register-form');
+let status = document.getElementById('status');
 form.addEventListener('submit', ev => {
     ev.preventDefault();
-    let data = new FormData(form);
+    let formData = new FormData(form);
+    if (formData.get("password") !== formData.get("password2"))
+        return status.innerText = "Passwords must match";
 
-    if (!(data.get('username') && data.get('password'))) {
-        console.log('There is empty fields');
-        return;
-    }
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/register', true);
-    xhr.onload = function (e) {
-        if (xhr.readyState === 4 && xhr.status === 200)
-            if (xhr.responseText === '200')
-                window.location = window.location.origin;
-            else {
-                console.log('Username taken');
-                form.reset();
-            }
+    sendRequest("POST", "/register", formData).then(resp => {
+        window.location = window.location.origin;
+    }).catch(resp => {
+        if (resp === "taken")
+            status.innerText = "Username is not available";
         else
-            console.log('Registration failed');
-    };
-    xhr.onerror = function (e) {
-        console.log('Registration failed');
-    };
-    xhr.send(data);
+            disableForm("Invalid registration token");
+        form.reset();
+    });
 });
+
+function getParameterByName(name) {
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(window.location.href);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+function sendRequest(method, url, data) {
+    return new Promise((res, rej) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url, true);
+
+        xhr.onload = e => {
+            if (xhr.readyState === 4 && xhr.status === 200)
+                res(xhr.responseText);
+            else
+                rej(e, xhr.statusText);
+        };
+        xhr.onerror = e => rej(xhr.statusText);
+        xhr.send(data);
+    });
+}
+function disableForm(warning) {
+    for (let input of form.querySelectorAll("input")){
+        input.disabled = true;
+        input.readOnly = true;
+    }
+    status.innerText = warning;
+}
+if (!getParameterByName("token")){
+    disableForm("No registration token");
+}
