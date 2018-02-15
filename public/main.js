2e304e9fd2a804219f2b8614d40702ac09f18cd2
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2018-02-15 12:18:11 +0100

    minor change in gen func

--- public/main.js
+++ public/main.js
@@ -129,7 +129,7 @@ function renderCategories() {
 function passgenModal() {
     sendRequest('GET', "/wordlists").then(languages => {
         ModalsJs.open(templates.passgenModal.render({ languages }));
-        let form = document.getElementById("passgen-type-row");
+        let row = document.getElementById("passgen-type-row");
         let phraseGen = document.getElementById("passphrase-gen");
         let wordGen = document.getElementById("password-gen");
         let newPass = document.getElementById("new-pass");
@@ -147,17 +147,15 @@ function passgenModal() {
             newPass.innerText = generatePassword(chars, len);
         };
 
-        form.on("click", "div.passgen-top-row-btn", ev => {
+        row.on("click", "div.passgen-top-row-btn", ev => {
+            row.querySelector(".active").classList.remove("active");
+            ev.target.classList.add("active");
             if (ev.target.dataset.type === "phrase"){
-                ev.target.classList.add("active");
-                form.querySelector("div[data-type=word").classList.remove("active");
                 phraseGen.classList.add("active");
                 wordGen.classList.remove("active");
                 newPassphraseFunc();
             }
             else if (ev.target.dataset.type === "word"){
-                ev.target.classList.add("active");
-                form.querySelector("div[data-type=phrase").classList.remove("active");
                 phraseGen.classList.remove("active");
                 wordGen.classList.add("active");
                 newPasswordFunc();
